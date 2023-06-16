22e396f5f5c33f27633f0cc12f1ea850ebcecbdb
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2023-06-16 14:54:07 +0200

    Update ringer.py

--- ringer.py
+++ ringer.py
@@ -4,32 +4,42 @@ from datetime import date
 
 
 def get_links(url):
-
     # Get url result
     result = requests.get(url)
 
     # Create a BeautifulSoup object to parse the HTML content
     soup = BeautifulSoup(result.content, "html.parser")
 
-    # Getting today's date
-    current_date = (date.today()).strftime('%m-%d-%Y')
-
     # Find the <main> element on the page
     main_element = soup.find("main")
 
-    # Find all <span> tags within the <main> element
-    a_tags = main_element.find_all("a", href=True)
+    # Create new variable headlines which finds the h2 elements on the page
+    headlines = main_element.find_all('h2')
 
     # Opening and naming file
-    f = open(f"news.txt {current_date}", "w")
+    f = open(f"NBA News.txt", "w")
 
-    # Write content into txt file
-    for num, tag in enumerate(a_tags):
-        f.write(f"""
-        Story {num + 1}
-        ------------------"
-        {tag.text.strip()}
-        {tag['href']}
-                """)
-    f.close()
+    for num,topic in enumerate(headlines):
+        # Check if an <a> tag exists within the <h2> tag
+        if topic.find('a'):
+            # Retrieve the href link using the 'a' tag within the 'h2' tag
+            link = topic.find('a')['href']
+
+            # Retrieve the text content of the 'h2' tag
+            text = topic.get_text()
+
+            # Begin writing to file
+            f.write(f"""
+                   STORY {num + 1}: {text}
+                   LINK: {link}
+                   ------------------------------------------------------
+                 """)
+        #  'TypeError: 'NoneType' object is not subscriptable' error will pop up if no href link is found so we need to handle error
+        else:
+            f.write(f"""
+            STORY {num + 1}: {text}
+            LINK: No link found for this story.
+            ------------------------------------------------------
+            """)
+
 
